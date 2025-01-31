60607a61aa24b67e71560a319519de9c06eca645
LSY0108 <seungye1209@gmail.com>
2025-01-31 22:52:32 +0900

    01/31 update

---
+++ build/precache-manifest.5715a7a07f70e5d0389c166960a1efeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6bcab708b9fac6dab1d07ff075ea1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dde793f6643906924c36",
+    "url": "/portfolio/static/css/main.f75d3be8.chunk.css"
+  },
+  {
+    "revision": "1083cc5016081e329f2e",
+    "url": "/portfolio/static/js/2.3544b682.chunk.js"
+  },
+  {
+    "revision": "dde793f6643906924c36",
+    "url": "/portfolio/static/js/main.d7339b33.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "3030f4ee7cf442df563cc2154e71b7bd",
+    "url": "/portfolio/static/media/Me1.3030f4ee.png"
+  },
+  {
+    "revision": "cd6490d6a57df5d41bab2052b5880202",
+    "url": "/portfolio/static/media/Me2.cd6490d6.png"
+  }
+]);
